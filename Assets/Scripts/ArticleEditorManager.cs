5fe4724d184b50088b9db7e05dbbe4eb8c49a3e0
Daria <sorokina.dasha@mail.ru>
2025-05-08 20:19:53 +0200

    Add tutorial dialogue

--- Assets/Scripts/ArticleEditorManager.cs
+++ Assets/Scripts/ArticleEditorManager.cs
@@ -40,7 +40,11 @@ public class ArticleEditorManager : MonoBehaviour
         bool ShouldBeEditedNext(Article article)
         {
             // here is logic for article selection
-            return true;
+            if(DialogueManager.instance.dialogueCompleted.ContainsKey(3))
+            {
+                return true;
+            }
+            return false;
         }
     }
     
